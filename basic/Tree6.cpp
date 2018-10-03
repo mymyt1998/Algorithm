a33a5727be111ba1266f32fd4d3f98f83eb04b8a
Evelyrn <30970176+mymyt1998@users.noreply.github.com>
2018-10-03 21:43:41 +0800

    层序遍历

--- basic/Tree6.cpp
+++ basic/Tree6.cpp
@@ -1,3 +1,13 @@
+class Node {//N叉树
+public:
+	int val;
+	vector<Node*> children;
+	Node() {}
+	Node(int _val, vector<Node*> _children) {
+		val = _val;
+		children = _children;
+	}
+};
 class Solution {//N叉树的层序遍历
 public:
 	vector<vector<int>> levelOrder(Node* root) {
@@ -7,7 +17,7 @@ public:
 		queue<Node*> nod;
 		Node* R;
 		nod.push(root);
-		while (!nod.empty())//按每层遍历完所有结点，存入时按个数放
+		while (!nod.empty())
 		{
 			vector<int> cur;
 			int len = nod.size();//当前层的结点个数
@@ -15,8 +25,8 @@ public:
 			{
 				R = nod.front();
 				nod.pop();
-				cur.push_back(R->val);
-				for (auto it : R->children)
+				cur.push_back(R->val);//存当前层节点
+				for (auto it : R->children)//放入下一层所有子节点
 					nod.push(it);
 			}
 			re.push_back(cur);
